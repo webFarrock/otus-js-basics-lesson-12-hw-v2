693b1a4f77d1f578e616121a9552a24ab5db7909
webfarrock <webfarrock@mail.ru>
2021-11-26 20:25:04 +0300

    Модуль для сохранения/получения истории поиска

---
+++ src/js/storage/localStorage.js
@@ -0,0 +1,25 @@
+const SEARCH_HISTORY_KEY = "SEARCH_HISTORY";
+const HISTORY_MAX_SIZE = 10;
+/**
+ * @return {any|*[]}
+ */
+const getSearchHistory = () => {
+  let history = localStorage.getItem(SEARCH_HISTORY_KEY);
+  if (!history) {
+    return [];
+  }
+  history = JSON.parse(history);
+
+  return Array.isArray(history) ? history : [];
+};
+
+const addToHistory = (cityName) => {
+  const history = getSearchHistory();
+  history.unshift(cityName);
+
+  const newHistory = history.filter((value, index, self) => self.indexOf(value) === index).slice(0, HISTORY_MAX_SIZE);
+
+  localStorage.setItem(SEARCH_HISTORY_KEY, JSON.stringify(newHistory));
+};
+
+export { getSearchHistory, addToHistory };
