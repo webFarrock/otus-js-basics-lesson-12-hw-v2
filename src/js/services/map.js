762b53d11cb08429b504c1dcb12fea635a4594c4
webfarrock <webfarrock@mail.ru>
2021-11-26 20:25:41 +0300

    Создание img для карты

--- src/js/services/map.js
+++ src/js/services/map.js
@@ -1,16 +1,16 @@
 const API_KEY = "AIzaSyBx0xC5JHjnOL8wu8UHKjjLIhxmqRDlbVM";
 const MAP_BASIC_URL = "//maps.googleapis.com/maps/api/staticmap";
-const MAP_ZOOM = 14;
+const MAP_ZOOM = 12;
 const MAP_WIDTH = "400";
 const MAP_HEIGHT = "400";
 
-const makeMapSrc = async (locationName) => {
-  return `${MAP_BASIC_URL}?key=${API_KEY}&center=${locationName}&zoom=${MAP_ZOOM}&size=${MAP_WIDTH}x${MAP_HEIGHT}`;
+const makeMapSrc = (lat, lon) => {
+  return `${MAP_BASIC_URL}?key=${API_KEY}&center=${lat},${lon}&zoom=${MAP_ZOOM}&size=${MAP_WIDTH}x${MAP_HEIGHT}`;
 };
 
-const createMapImage = (locationName) => {
+const createMapImage = (lat, lon) => {
   const img = new Image();
-  img.src = makeMapSrc(locationName);
+  img.src = makeMapSrc(lat, lon);
   img.width = MAP_WIDTH;
   img.height = MAP_HEIGHT;
 
