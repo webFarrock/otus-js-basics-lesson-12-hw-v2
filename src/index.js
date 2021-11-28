c6db670e0b3f3755f2a4210041ddc2f62ed3323f
webfarrock <webfarrock@mail.ru>
2021-11-28 06:28:11 +0300

    test changes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "./styles/style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { initApp } from "./js/forecast";
 
+console.log("test changes");
 const APP_ELEM_ID = "app";
 const appEl = document.getElementById(APP_ELEM_ID);
 
