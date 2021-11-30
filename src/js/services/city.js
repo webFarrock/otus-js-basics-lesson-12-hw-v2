6e2e88f6e603d50440ee703b52f0ce82b78c192c
webfarrock <webfarrock@mail.ru>
2021-11-30 16:29:39 +0300

    refactor getCurrentCity by geojs

--- src/js/services/city.js
+++ src/js/services/city.js
@@ -1,52 +1,15 @@
 import axios from "axios";
 
-const API_KEY = "330e63a5-aa91-4873-a92c-181ef54c5bc0";
-const DEFAULT_USER_COORDS = [40.730885, -73.997383];
-const DEFAULT_USER_CITY = "New York, US";
+const GEO_JS_GET_PATH = "https://get.geojs.io/v1/ip/geo.json";
 
-const geocoderApiInstance = axios.create({
-  baseURL: "//geocode-maps.yandex.ru/1.x/",
-  params: {
-    apikey: API_KEY,
-    format: "json",
-    lang: "ru_RU",
-    results: 1,
-    kind: "locality",
-  },
-});
-
-const getCityNameByCoords = async (lat, lng) => {
+const getCurrentCity = async () => {
   try {
-    const result = await geocoderApiInstance.get(`/?geocode=${lat},${lng}`);
-    return result.data?.response?.GeoObjectCollection?.featureMember[0]?.GeoObject?.description || null;
+    const res = await axios.get(GEO_JS_GET_PATH);
+    return res.data.city;
   } catch (e) {
-    console.warn("Error while define user city. Use default city", e);
+    console.error(`error while get geo js data: `, e);
     return null;
   }
 };
 
-const getUserCurrentCoords = async () => {
-  let position = null;
-  try {
-    position = await new Promise((resolve, reject) => {
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(resolve, reject);
-      } else {
-        reject();
-      }
-    });
-
-    return [position.coords.latitude, position.coords.longitude];
-  } catch (e) {
-    console.warn("Error while define user coords. Use default city");
-    return DEFAULT_USER_COORDS;
-  }
-};
-
-const getCurrentCity = async () => {
-  const userCoords = await getUserCurrentCoords();
-  const userCityName = (await getCityNameByCoords(...userCoords)) || DEFAULT_USER_CITY;
-  return userCityName;
-};
-
-export { getCurrentCity };
+export { getCurrentCity, GEO_JS_GET_PATH };
